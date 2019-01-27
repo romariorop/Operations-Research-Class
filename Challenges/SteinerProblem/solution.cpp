b49d39774511d8b0b7a16f2a2637967f91b59aaa
rop <rop@ic.ufal.br>
2019-01-27 04:14:16 -0300

    Steiner Problem

--- Challenges/SteinerProblem/solution.cpp
+++ Challenges/SteinerProblem/solution.cpp
@@ -1,92 +1,118 @@
 #include <stdio.h>
 #include <ilcplex/ilocplex.h>
 #include <vector>
+#include <utility>
 using namespace std;
 
-const int MAX = 510;
-
+const int MAX = 50;
+int isTerminal[MAX];
 int main() {
 
     IloEnv env;
     IloModel model(env, "Steiner Problem");
     IloCplex cplex(model);
 
-    int isTerminal[MAX];
-    int cost[MAX][MAX];
+    
     int v, e, t, v1, v2, w;
-    int start, top;
+    pair<int, int> edges[MAX];
+    int cost[MAX];
 
     scanf("%d %d", &v, &e);
     for(int i = 0; i < e; ++i){
-        scanf("%d %d %d", &w, &v1, &v2);
+        scanf("%d %d %d", &v1, &v2, &w);
         --v1,--v2; // enum from 0 to v-1
-        cost[v1][v2] = cost[v2][v1] = w;
+        edges[i].first = v1;
+        edges[i].second = v2;
+        cost[i] = w;
     }
 
     scanf("%d", &t);
     for(int i = 0; i < t; ++i){
         scanf("%d", &v1);
+        --v1;
         isTerminal[v1] = 1;
     }
+    
+   // decision variables
+   IloBoolVarArray eTree(env, e);
+   IloBoolVarArray vTree(env, v);
+   IloArray<IloNumVarArray> flow(env, e);
 
-    // decision variables
-
-    IloArray<IloIntVarArray> path(env, v);
-    IloBoolVarArray onAns(env, v);
-    for(int i = 0; i < v; ++i)path[i] = IloIntVarArray(env, v, 0, 1);
-
-    //restrictions
-        
-        //graph restrictions
-        for(int i = 0; i < v; ++i){
-            model.add(path[i][i] == 0);
-            for(int j = 0; j < v; ++j){
-                model.add(path[i][j] == path[j][i]);   
-            }
+   for(int i = 0; i < e; ++i)flow[i] = IloNumVarArray(env, 2, 0, IloInfinity);
+   
+         // restrictions
+        // every terminal must be on tree
+        IloExpr totalTerminals(env);
+        for(int i = 0; i < v; ++i)if(isTerminal[i]){
+            totalTerminals+=vTree[i];
         }
-
-        //all terminals must be in answer
-        IloExpr totalTerminals(env);    
-        for(int i = 0; i < v; ++i)totalTerminals += onAns[i] * isTerminal[i];
         model.add(totalTerminals == t);
-
-        //all terminals must be linked to someone (unless there is only 1 terminal)
-    
-        for(int j = 0; j < v; ++j) {
-            IloExpr totalLinks(env);
-            for(int i = j+1; i < v; ++i){
-                totalLinks += path[j][i];
-            }
-        }
-
-        // must be a tree
-        IloExpr totalEdges(env);
-        IloExpr totalVertex(env);
-        for(int i = 0; i < v; ++i)totalVertex+= onAns[i];
+        // terminal must have at least one edge && non-termials must have at least two connections
         for(int i = 0; i < v; ++i){
-            for(int j = i+1; j < v; ++j){
-                totalEdges += path[i][j];
+            if(isTerminal[i]) {
+                IloExpr total(env);
+                for(int j = 0; j < e; ++j){
+                    if(edges[j].first == i || edges[j].second == i){
+                        total+= eTree[j];
+                    }
+                }
+                model.add(total >= 1);
+            } else {
+                IloExpr total(env);
+                for(int j = 0; j < e; ++j){
+                    if(edges[j].first == i || edges[j].second == i){
+                        total+= eTree[j];
+                    }
+                }
+                model.add(total >= 2 * vTree[i]);
             }
         }
-        totalVertex-= 1;
-        model.add(totalEdges == totalVertex);
-    
-    //obj function
+        // must be a tree
+        IloExpr totalVertex(env);
+        IloExpr totalEdges(env);
+        for(int i = 0; i < e; ++i){
+            totalEdges += eTree[i];
+        }
+        for(int i = 0; i < v; ++i){
+            totalVertex+= vTree[i];
+        }
+        model.add(totalVertex == totalEdges + 1);
+        // acyclic
+        for(int i = 0; i < e; ++i){
+            model.add(flow[i][0] + flow[i][1] == (2 * eTree[i])); 
+        }
+        
+        IloExprArray flowsSum(env, v);
+      
+        for(int i = 0; i < v; ++i)flowsSum[i] = IloExpr(env);
+        for(int i = 0; i < e; ++i){
+            flowsSum[edges[i].first] += flow[i][0];
+            flowsSum[edges[i].second] += flow[i][1];
+        }
+        for(int i = 0; i < v; ++i){
+            model.add(flowsSum[i] <= (2.0 - 2.0/v));
+        }
+
+
+ 
+   //objective function
 
     IloExpr totalCost(env);
-    for(int i = 0; i < v; ++i){
-        for(int j = i+1; j < v; ++j){
-            totalCost+= path[i][j] * cost[i][j];
-        }
-    }
+    for(int i = 0; i < e; ++i)totalCost += eTree[i] * cost[i];
     model.add(IloMinimize(env, totalCost));
-
-    // results
+   //results
 
     cplex.solve();
 
     printf("Minimum Cost = %.0lf\n", cplex.getObjValue());
+    
+    IloNumArray solutions(env, e);
+    cplex.getValues(solutions, eTree);
+    for(int i = 0; i < e; ++i){
+        if(solutions[i])printf("%d - %d\n", edges[i].first+1, edges[i].second+1);
+    }
 
     env.end();
+
     return 0;
 }
